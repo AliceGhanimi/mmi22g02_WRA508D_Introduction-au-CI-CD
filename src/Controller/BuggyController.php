49defb97c5697b02a7e13ab70a59abe45d4a3aa9
Ghantfasia <aliceghanimi@hotmail.com>
2025-02-25 12:02:29 +0100

    update

--- src/Controller/BuggyController.php
+++ src/Controller/BuggyController.php
@@ -13,9 +13,11 @@ class BuggyController extends AbstractController
     {
         $data = $this->getData();
 
+        $message = $data['nonexistent_key'] ?? 'Je ne bug plus !';
         return $this->render('welcome.html.twig', [
-            'data' => $data['nonexistent_key'], // Bug : accès à une clé inexistante
-        ]);
+        	'message' => $message,
+    	]);
+
     }
 
     private function getData(): array
